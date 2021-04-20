ab0f0230527197710baebf9550f21f56560a5499
rutzera <adrian.rutzer@gmail.com>
2021-04-20 15:58:53 +0200

    changed picom.conf

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -7,27 +7,13 @@ function run {
   fi
 }
 
-#$HOME/.config/polybar/launch.sh &
-
-
-#Set your native resolution IF it does not exist in xrandr
-#More info in the script
-#run $HOME/.config/qtile/scripts/set-screen-resolution-in-virtualbox.sh
-
-#Find out your monitor name with xrandr or arandr (save and you get this line)
-#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
-#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
-#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
-#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 
 #change your keyboard if you need it
 #setxkbmap -layout be
 
-#IN BETA PHASE
-#start sxhkd to replace Qtile native key-bindings
-#run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
-run sxhkd -c $HOME/.config/sxhkd/sxhkdrc &
-picom --config $HOME/.config/bspwm/picom.conf &
+#run sxhkd -c $HOME/.config/sxhkd/sxhkdrc &
+#$HOME/.config/polybar/launch.sh &
+#picom --config $HOME/.config/bspwm/picom.conf &
 
 #starting utility applications at boot time
 run variety &
@@ -43,6 +29,8 @@ blueberry-tray &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 
+# LAPTOP settings
+# laptop trackpad settings
 libinput-gestures-setup start &
-
+# handling external desktop 
 sleep 0.5s && autorandr --change &
